44439f43d6a0353de1198d3229a9198c3c246547
Woopser <petittom222@gmail.com>
2022-11-09 15:39:23 -0500

    message Err
    measaeda

--- app/Http/Requests/FilmRequest.php
+++ app/Http/Requests/FilmRequest.php
@@ -29,8 +29,7 @@ class FilmRequest extends FormRequest
             'cote' => 'required|min:0.1|max:10',
             'realisateur' =>'required|min:2|max:200',
             'genre' => 'required|min:2|max:200',
-            'image' => '',
-            //Conitnuer
+            'image' => 'required',
         ];
     }
 
@@ -39,6 +38,9 @@ class FilmRequest extends FormRequest
             'nom.required' => 'Le titre du film est obligatoire.',
             'nom.min' => 'le titre doit avoir 2 caractères.',
             'nom.max' => 'Le titre ne doit pas avoir plus de 2000 caractères',
+            'duree.required' => 'Le film doit avoir une durée.',
+            'duree.min' => 'Le film doit avoir une durée de plus que 1 minute.',
+            'cote.required' => 'La film doit avoir une cote',
         ];
     }
 }
